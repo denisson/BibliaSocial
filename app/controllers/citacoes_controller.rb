8617894e98eebd37ec96e27248ef354310af1607
unknown <root@.(none)>
2012-02-04 20:58:05 -0200

    Mudanca no Layou e nas funcionalidades

---
+++ app/controllers/citacoes_controller.rb
@@ -0,0 +1,15 @@
+﻿class CitacoesController < ApplicationController
+
+  def index
+	@versiculo = Versiculo.find(params[:versiculo_id])
+	@citacoes = @versiculo.citacoes
+	
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+ 
+end
+
+
